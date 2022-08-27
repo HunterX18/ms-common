03460610212811db9ff4869751fcbf3cf6f4634f
HunterX18 <srsahu13@gmail.com>
2022-08-27 12:10:47 +0530

    Updates

--- src/events/ticket-updated-event.ts
+++ src/events/ticket-updated-event.ts
@@ -1,11 +1,11 @@
 import { Subjects } from "./subjects";
 
-export interface TicketUpdateEvent {
+export interface TicketUpdatedEvent {
     subject: Subjects.TicketUpdated;
     data: {
         id: string;
         title: string;
-        price: string;
+        price: number;
         userId: string;
     }
 }
